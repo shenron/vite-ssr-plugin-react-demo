878dfc4fe55239311f048ff8f0f85b8db2a7cdb3
jerome.birembaut@gmail.com <jerome.birembaut@gmail.com>
2021-04-18 15:23:18 +0200

    Add scss test on About

--- client/pages/About.jsx
+++ client/pages/About.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import classNames from 'classnames'
+import styles from './About.scss'
 
 export default function About(props) {
+
+  const testClass = classNames(styles['test'], {
+    [styles['red']]: true,
+    [styles['bold']]: false
+  });
+
   return (
     <>
-      <h1>About</h1>
+      <h1><span className={testClass}>red</span> About</h1>
       <p>{JSON.stringify(props, null, 2)}</p>
     </>
   )
