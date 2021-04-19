de262887a26dbe54cc7a4af5290853626d271e94
Thomas CASTELLY <thomas@tcy.io>
2021-04-20 00:56:42 +0200

    install graphql, in progress ...

---
+++ back/graphql.mjs
@@ -0,0 +1,18 @@
+import { graphql, buildSchema } from 'graphql';
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => 'Hello world!',
+};
+
+// Run the GraphQL query '{ hello }' and print out the response
+graphql(schema, '{ hello }', root).then((response) => {
+  console.log(response);
+});
